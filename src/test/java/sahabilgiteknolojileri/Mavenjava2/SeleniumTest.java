144684f4f815401477fcbf24591ed967e8e3efdb
arca <aarcasahin@gmail.com>
2020-12-29 01:31:32 +0300

    first commit

---
+++ src/test/java/sahabilgiteknolojileri/Mavenjava2/SeleniumTest.java
@@ -0,0 +1,15 @@
+package sahabilgiteknolojileri.Mavenjava2;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test (groups= {"Smoke"})
+	public void FamilyMembers()
+	{
+		System.out.println("ARCA, DURAN, TUĞÇE, SANİYE");
+	}
+}
+
+//9d9e0b04ddc34262b4c5b890c36b6b1d  
+//admin-password for jenkins
---
+++ src/test/java/sahabilgiteknolojileri/Mavenjava2/SeleniumTest2.java
@@ -0,0 +1,13 @@
+package sahabilgiteknolojileri.Mavenjava2;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest2 {
+
+	
+	@Test (groups= {"Smoke"})
+	public void FamilyMembersAges()
+	{
+		System.out.println("24, 56, 29, 49");
+	}
+}
